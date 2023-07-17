ead74f0ea8c2bab414cace7c55886674eaac5fa9
xtianmarucco <christian.marucco@gmail.com>
2023-07-17 10:23:59 -0300

    add to do feature

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,5 +4,46 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  return <div>Add To Do</div>
+
+  //this is generic types for typescript
+  const [input, setInput] = useState<string>('')
+  const [todos, setTodos] = useState<string[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if(inputRef.current){inputRef.current.focus()}
+  }, [])
+
+  const handelSubmission = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log('form has been submitted')
+    if (input.trim() !== ''){
+      setTodos([...todos, input])
+      setInput('')
+    }
+  }
+  console.log(input)
+  console.log(todos)
+
+  return (
+    <form onSubmit={handelSubmission}>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input
+          ref={inputRef}
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          placeholder="start typing ..."
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+        
+      </div>
+    </form>
+  )
 }
